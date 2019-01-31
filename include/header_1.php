b0664a13e08b0ed67c5a1c27c77d007e536eebad
Spicate <46818887+Spicate@users.noreply.github.com>
2019-01-31 12:48:29 +0100

    Add files via upload

---
+++ include/header_1.php
@@ -0,0 +1,14 @@
+<div class="header-global">
+	<a href="https://youtube.com" class="logo" ></a>
+	<label for="show-menu" class="show-menu">Show Menu</label>
+	<input type="checkbox" id="show-menu" role="button">
+	<nav class="menu-global-nav">
+		<ul class="menu-global-ul">
+			<li class="menu-global-li accueil"><a href="index.php" class="test">Accueil</a></li>
+			<li class="menu-global-li Historique"><a href="historic.php" class="test">Historique</a></li>
+			<li class="menu-global-li download"><a href="download.php" class="test">Téléchargement</a></li>
+			<li class="menu-global-li media"><a href="media.php" class="test">Médias</a></li>
+		</ul>
+	</nav>
+	<div class="tempo"></div>
+</div>
