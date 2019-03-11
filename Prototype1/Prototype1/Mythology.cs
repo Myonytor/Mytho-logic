97c4f50a0328cdbdc5a5c238868d12141767f89a
Emmanuel Mollard <emmanuel.mollard@epita.fr>
2019-03-11 17:57:47 +0100

    update Mythology

--- Prototype1/Prototype1/Mythology.cs
+++ Prototype1/Prototype1/Mythology.cs
@@ -1,14 +1,44 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Prototype1
 {
     public class Mythology
     {
-        private List<Monster> bestiaries;
+        private string name;
+        private List<Monster> bestiary;
 
-        public Mythology()
+        public Mythology(int nbrBestiary, Player p)
         {
-            bestiaries = new List<Monster>();
+            bestiary = new List<Monster>();
+            ReadBestiary(nbrBestiary, p);
+        }
+
+        private void ReadBestiary(int nbrBestiary, Player p)
+        {
+            string file = "../../../../bestiaries";
+
+            string[] bestiaries = new StreamReader(file).ReadToEnd().Split('\n');//Tout les bestiaires
+
+            if (nbrBestiary<0 || nbrBestiary >= bestiaries.Length)
+                throw new Exception("");
+            
+            string[] monsters = bestiaries[nbrBestiary].Split('\t');//Tout les monstres
+
+            {//TODO recuperer pouvoir special de la mythology
+                string[] stats = monsters[0].Split('$');
+                name = stats[0];
+            }
+
+            for(int i = 1; i < monsters.Length; i++)
+            {
+                string[] stats = monsters[i].Split('$');
+                
+                Monster m = new Monster(stats[0], p, Int32.Parse(stats[1]), Int32.Parse(stats[2]), 0, 0 );
+                
+                bestiary.Add(m);
+            }
         }
     }
 }
