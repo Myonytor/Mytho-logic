7e266379663395b3c31512a36011a478e59557c1
thibault trembleau <thibault.trembleau@epita.fr>
2019-03-14 10:33:18 +0100

    Unity projet test

---
+++ Unity/Mytho-Logic/Assets/Script/Board.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public class Board : MonoBehaviour
+{
+    public GameObject hexPrefab;
+    public GameObject[,] hexGrid;
+
+    int width = 10;
+    int height = 10;
+
+    float xOffset = 0.8f;
+    float yOffset = 0.24f;
+
+    // Start is called before the first frame update
+    public void Setup()
+    {
+        hexGrid = new GameObject[width, height];
+        for (int y = 0; y < width; y++)
+        {
+            for (int x = 0; x < height; x++)
+            {
+                float xPos = x * xOffset + xOffset * y;
+                float yPos = y * yOffset - yOffset * x;
+                GameObject hex = Instantiate(hexPrefab, new Vector2(xPos, yPos), Quaternion.identity) as GameObject;
+                hex.transform.parent = transform;
+                hex.name = "Hex_x" + x + "_y" + y;
+                hexGrid[x, y] = hex;
+            }
+        }
+    }
+}
---
+++ Unity/Mytho-Logic/Assets/Script/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Board board;
+    // Start is called before the first frame update
+    void Start()
+    {
+        board.Setup();
+        //board.hexGrid[1, 2].GetComponentInChildren<SpriteRenderer>().color = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
