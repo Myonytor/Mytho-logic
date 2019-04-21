38c3926cd0c66f73e957143f9e9f0a97cfd2b9fa
Spicate <46818887+Spicate@users.noreply.github.com>
2019-04-21 15:09:14 +0200

    Ajout visuel des spawns

--- Unity/Assets/Script/Board.cs
+++ Unity/Assets/Script/Board.cs
@@ -14,9 +14,13 @@ public class Board : MonoBehaviour
     float xOffset = 0.8f;
     float yOffset = 0.24f;
 
+    Vector2 positionSpawn1 = new Vector2(0.75f, -2f);
+    Vector2 positionSpawn2 = new Vector2(12.06f, 1.5f);
+
     // Start is called before the first frame update
     public void Setup()
     {
+        //Apparition du Plateau
         hexGrid = new GameObject[width, height];
         for (int y = 0; y < width; y++)
         {
@@ -30,5 +34,41 @@ public class Board : MonoBehaviour
                 hexGrid[x, y] = hex;
             }
         }
+
+        //Apparition du Spawn 1
+        GameObject Spawn1 = new GameObject("Spawn1");
+        Spawn1.transform.parent = transform;
+        for (int spawnY = 0; spawnY < 3; spawnY++)
+        {
+            for (int spawnX = 0; spawnX < 3; spawnX++)
+            {
+                if (!(spawnX == 0 && spawnY == 0) && !(spawnX == 2 && spawnY == 2))
+                {
+                    float xPos = spawnX * xOffset + xOffset * spawnY;
+                    float yPos = spawnY * yOffset - yOffset * spawnX;
+                    GameObject hexSpawn = Instantiate(hexPrefab, new Vector2(xPos,yPos), Quaternion.identity, Spawn1.transform) as GameObject;
+                    hexSpawn.name = "Hex_x" + spawnX + "_y" + spawnY;
+                }
+            }
+        }
+        Spawn1.transform.position = positionSpawn1;
+
+        //Apparition du Spawn 2
+        GameObject Spawn2 = new GameObject("Spawn2");
+        Spawn2.transform.parent = transform;
+        for (int spawnY = 0; spawnY < 3; spawnY++)
+        {
+            for (int spawnX = 0; spawnX < 3; spawnX++)
+            {
+                if (!(spawnX == 0 && spawnY == 0) && !(spawnX == 2 && spawnY == 2))
+                {
+                    float xPos = spawnX * xOffset + xOffset * spawnY;
+                    float yPos = spawnY * yOffset - yOffset * spawnX;
+                    GameObject hexSpawn = Instantiate(hexPrefab, new Vector2(xPos, yPos), Quaternion.identity, Spawn2.transform) as GameObject;
+                    hexSpawn.name = "Hex_x" + spawnX + "_y" + spawnY;
+                }
+            }
+        }
+        Spawn2.transform.position = positionSpawn2;
     }
 }
