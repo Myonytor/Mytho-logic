7e2fbad1c49d1f875b8ad1f154e7021c9b7f93b9
Marion <marion.bouvier@epita.fr>
2019-04-23 20:06:25 +0100

    remise en place de Unit

--- Unity/Assets/Script/Tile.cs
+++ Unity/Assets/Script/Tile.cs
@@ -6,10 +6,14 @@ public class Tile : MonoBehaviour
 {
     Board boardParent;
     public Vector2 coordinate;
+    public GameObject monster;
+    public bool isEmpty;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        isEmpty = true;
+        monster = null;
     }
 
     // Update is called once per frame
