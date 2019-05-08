258aa19a3864a2f8cdb1431c0ed4150e0f90ebf0
Marion <marion.bouvier@epita.fr>
2019-05-08 15:08:22 +0100

    ajout dans le spawn si la case est vide

--- Unity/Assets/Script/Unit.cs
+++ Unity/Assets/Script/Unit.cs
@@ -34,36 +34,6 @@ public class Unit : MonoBehaviour
 		power = 2;
 	}
 
-	/*public void Add(string name, string tagSpawn) // give in parameter the monster and player
-	{
-		GameObject[] spawn = GameObject.FindGameObjectsWithTag(tagSpawn);
-
-		GameObject monster = Instantiate(prefabMonster, spawn[3].transform.position, Quaternion.identity) as GameObject;
-		spawn[3].GetComponent<Tile>().isEmpty = false;
-		monster.transform.parent = transform;
-		monster.name = name;
-		monster.tag = "Monster";
-		monster.SetActive(true);
-		
-		monsters.Add(monster);
-
-		Debug.Log("Add a monster in the map");
-	}
-
-	public void Move(Vector3 direction, Vector3 position) // to change for a lot of move
-	{
-		int i = 0;
-		foreach (Vector3 position in positions)
-		{
-			Vector3 direcion = directions[i]
-		 
-			GameObject monster = monsters.Find(x => x.transform.position == position);
-			monster.transform.position = direction;
-		
-			Debug.Log(monster.name + " move");
-		}
-	}*/
-
 	public enum State
 	{
 		DEAD,
