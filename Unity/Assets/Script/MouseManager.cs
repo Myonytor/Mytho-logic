c3a05f360de8b9dbc380e70f78cf6271129708f9
Emmanuel Mollard <emmanuel.mollard@epita.fr>
2019-05-14 15:41:07 +0100

    update MouseManager

--- Unity/Assets/Script/MouseManager.cs
+++ Unity/Assets/Script/MouseManager.cs
@@ -97,5 +97,7 @@ public class MouseManager : MonoBehaviour
     public void Clear()
     {
         unit = null;
+        ClearSelection(hoveredObject);
+        ClearSelection(selectedObject);
     }
 }
