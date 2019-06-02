313166ac141c81dbc1077ef4e9bc8ddb61e24b03
Thibault Trembleau <thibault.trembleau@epita.fr>
2019-06-02 18:13:51 +0200

    Ajout selection pseudo

---
+++ Unity/Assets/Script/PlayerRegistrationMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerRegistrationMenu : MonoBehaviour
+{
+    public GameObject usernameGameobject;
+    private string username;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        username = usernameGameobject.GetComponent<InputField>().text;
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if(username != "")
+            {
+                RegisterButton();
+            }
+        }
+    }
+
+    public void RegisterButton()
+    {
+        Debug.Log("Success Username");
+    }
+}
