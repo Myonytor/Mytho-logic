d43079e0fd8526a986b949515db78d322c3cf745
thibault trembleau <thibault.trembleau@epita.fr>
2019-06-04 17:36:38 +0200

    playerregistration loading screen

--- Unity/Assets/Script/PlayerRegistrationMenu.cs
+++ Unity/Assets/Script/PlayerRegistrationMenu.cs
@@ -75,7 +75,8 @@ public class PlayerRegistrationMenu : MonoBehaviour
 
         while (!operation.isDone)
         {
-            Debug.Log(operation.progress);
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            Debug.Log("Chargement : " + progress);
             yield return null;
         }
     }
