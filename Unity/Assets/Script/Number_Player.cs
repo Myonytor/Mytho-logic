ae52d8c70e837375fc192caaad58fcc5ae2e67b5
Léa Cruciani <lea.cruciani@epita.fr>
2019-06-04 17:36:39 +0200

    Network menu

---
+++ Unity/Assets/Script/Number_Player.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Number_Player : NetworkBehaviour
+{
+    private int numOfConnectedPlayers = Network.connections.Length;
+}
