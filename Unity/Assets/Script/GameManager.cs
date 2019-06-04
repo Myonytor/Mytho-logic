33e8d1770767d5892aff305d47090dee9c72f5d5
Marion <marion.bouvier@epita.fr>
2019-06-04 19:17:49 +0100

    ajout de commentaire et suppression des debug

--- Unity/Assets/Script/GameManager.cs
+++ Unity/Assets/Script/GameManager.cs
@@ -107,7 +107,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape)) // Détecter le bouton échap
+        if (Input.GetKeyDown(KeyCode.Escape)) // Détecter le bouton échap correspondant au bouton pause
         {
             if (pauseMenu.activeSelf)
                 Resume();
@@ -115,7 +115,7 @@ public class GameManager : MonoBehaviour
                 Pause();
         }
 
-        if (!pauseMenu.activeSelf && Input.GetKeyDown(KeyCode.Space))
+        if (!pauseMenu.activeSelf && Input.GetKeyDown(KeyCode.Space)) // Détecter le bouton espace correspondant au bouton passer
         {
             if(newTurnPanel.activeSelf)
                 StartNewTurn();
@@ -123,7 +123,7 @@ public class GameManager : MonoBehaviour
                 skipTurnFunc();
         }
 
-        if((int)(decompte - Time.deltaTime) != (int)(decompte))
+        if((int)(decompte - Time.deltaTime) != (int)(decompte)) // Affiche le temps qui s'écoule en seconde
         {
             timeText.text = "" + (int)(decompte - Time.deltaTime);
         }
@@ -134,10 +134,12 @@ public class GameManager : MonoBehaviour
             mouse.Clear();
             decompte = timer;
             if (indexPlayer == 1 || PlayerPrefs.GetInt("online") != 0)
+                // Si le tour du deuxième joueur vient de se finir où le jeu se déroule sur le même ordinateur
             {
                 NextBoard();
                 int w = 0;
-                for(int i = 0; i < Players.Count; i++)
+                
+                for(int i = 0; i < Players.Count; i++) // Vérifie si il y a un gagnant
                 {
                     foreach (var m in Players[i]._monsters)
                     {
@@ -152,7 +154,7 @@ public class GameManager : MonoBehaviour
                         i = Players.Count;
                 }
 
-                if (Math.Abs(w) == 3)
+                if (Math.Abs(w) == 3) // Affichage du gagnant et du menu qui va avec
                 {
                     AudioManager.GetComponent<AudioManager>().Play("Fireworks");
                     endGame.text = (w > 0 ? Players[0].Name : Players[1].Name) + " WIN !";
@@ -161,12 +163,13 @@ public class GameManager : MonoBehaviour
                 }
             }
 
-            if (PlayerPrefs.GetInt("online") == 0)
+            if (PlayerPrefs.GetInt("online") == 0) // Si le jeu se déroule sur un même ordinateur il y a un changement de l'index du joueur
             {
                 indexPlayer = indexPlayer == 0 ? 1 : 0;
                 mouse.player = Players[indexPlayer];
             }
 
+            // Changement de l'affichage après changement du joueur
             skipTurn = false;
             ChangeSpriteButton();
             if(!endGamePanel.activeSelf)
@@ -179,12 +182,13 @@ public class GameManager : MonoBehaviour
             
             DisplayTime = DisplayTime - Time.deltaTime;
 
+            // Affiche les choix des mythologies des deux joueurs au début du premier tour
             if (currentLangage == 0)
             {
                 Display1.text = Players[0].Name + " a choisi la mythologie " + Players[0].Mythologie.Name;
                 Display2.text = Players[1].Name + " a choisi la mythologie " + Players[1].Mythologie.Name;
             }
-            else
+            else // Traduction en anglais
             {
                 for (int i = 0; i < 2; i++)
                 {
@@ -217,7 +221,6 @@ public class GameManager : MonoBehaviour
                 }
             }
         }
-
         else
         {
             TDisplay.SetActive(false);
@@ -293,8 +296,6 @@ public class GameManager : MonoBehaviour
         {
             if (monsters.Value.Count == 2) // S'il y a deux monstres qui veulent aller sur cette case
             {
-                Debug.Log("Deux monstres se retrouvent sur la même case");
-
                 int power = 0;
                 var m = monsters.Value;
                 int attack = m[0].Power - m[1].Power;
@@ -306,11 +307,7 @@ public class GameManager : MonoBehaviour
                 if (attacks.ContainsKey(monsters.Key)) // S'il y a des monstres qui attaquent cette case
                 {
                     foreach (var monster in attacks[monsters.Key])
-                    {
                         attack += (monster.Player == m[0].Player ? monster.Power : -monster.Power);
-                    }
-
-                    Debug.Log("Il y a des attaques extérieur");
                 }
 
                 var movement = m[1]._movement;
@@ -354,9 +351,7 @@ public class GameManager : MonoBehaviour
                 {
                     m[0]._position = monsters.Key;
                     Move(m[0]);
-                    Debug.Log("Il ne reste plus que le monstre " + m[0].Name);
                 }
-                else Debug.Log("Il y a toujours deux monstres");
             }
             else // Sinon, il n'y a qu'un monstre qui veut atteindre cette case
             {
@@ -370,15 +365,10 @@ public class GameManager : MonoBehaviour
                     UsePowerSpecial(m, Mythologie.Mytho.Nordique, ref attack);
 
                     foreach (var monster in attacks[monsters.Key])
-                    {
                         attack += (monster.Player == m.Player ? monster.Power : -monster.Power);
-                    }
 
                     if (attack <= 0) // Si le monstre perd
-                    {
-                        if (m.wounded) Debug.Log(m.Name + " est mort d'attaque extérieur");
                         State(m);
-                    }
                 }
             }
         }
@@ -434,14 +424,11 @@ public class GameManager : MonoBehaviour
     // Met en place les images des monstres ainsi que les noms qui leur sont liés sur les boutons d'ajout des monstres
     public void ChangeSpriteButton()
     {
-        Debug.Log(Buttons.Count);
         for(int i = 0; i < Buttons.Count; i ++)
         {
             Buttons[i].GetComponent<UnityEngine.UI.Image>().sprite = Players[indexPlayer].Mythologie
                 .Monsters[i * 2 % Players[indexPlayer].Mythologie.Monsters.Count].GetComponent<SpriteRenderer>().sprite;
             Buttons[i].GetComponent<NomPerso>().ChangePlayer(indexPlayer, (int) Players[indexPlayer].Mythologie.Name);
-            
-            Debug.Log(Buttons[i].GetComponent<NomPerso>().player + ", " + Buttons[i].GetComponent<NomPerso>().CurrentMythology);
         }
 
         ButtonMythology.GetComponent<UnityEngine.UI.Image>().sprite =
