85dd5be5b99afc58d918798e90db05f21bc32e1b
Emmanuel <emmanuel.mollard@epita.fr>
2019-06-04 15:40:04 +0100

    Update PlayerRegistrationMenu

--- Unity/Assets/Script/PlayerRegistrationMenu.cs
+++ Unity/Assets/Script/PlayerRegistrationMenu.cs
@@ -18,7 +18,7 @@ public class PlayerRegistrationMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
+        if (Input.GetKeyDown(KeyCode.Return) && UsernameChoice.activeSelf)
         {
             RegisterButton();
         }
